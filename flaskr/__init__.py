6c6433773fa6e9fdc8e978f4f957ab0a3638555b
ycl <yuanchenglei0530@126.com>
2018-12-12 16:12:24 +0800

    change structure

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,30 +1,6 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from config import config
-import os
+from flask_login.login_manager import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-
-
-def create_app():
-    app = Flask(__name__,
-                template_folder="../templates",
-                static_folder="../static")
-
-    from . import auth
-    app.register_blueprint(auth.bp)
-    from . import blog
-    app.register_blueprint(blog.bp)
-
-    app.config.from_object(config[os.getenv('FLASK_ENV')])
-
-    db.init_app(app)
-
-
-    login_manager.init_app(app)
-
-    return app
-
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,22 @@
-from flaskr import create_app
+from flask import Flask
+from config import config
+import os
+from flaskr import db, login_manager
 
+app = Flask(__name__,
+            template_folder="templates",
+            static_folder="static")
+
+from flaskr import auth
+app.register_blueprint(auth.bp)
+from flaskr import blog
+app.register_blueprint(blog.bp)
+
+app.config.from_object(config[os.getenv('FLASK_ENV')])
+
+db.init_app(app)
+
+login_manager.init_app(app)
 
 
 if __name__ == '__main__':
